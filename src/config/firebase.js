bd751504fdc0bf425624efff8fe2ce6cebd1c6fc
elnur-elnur <tahirzadee@gmail.com>
2023-04-13 13:37:15 +0400

    config firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUYcynU9Lid9jToQUxt-qDGDC05J7EM3c",
+  authDomain: "time-logg-app.firebaseapp.com",
+  projectId: "time-logg-app",
+  storageBucket: "time-logg-app.appspot.com",
+  messagingSenderId: "1004929905027",
+  appId: "1:1004929905027:web:4c23387a1bce391dfec6b6",
+  measurementId: "G-KT1SVYTWGL",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
