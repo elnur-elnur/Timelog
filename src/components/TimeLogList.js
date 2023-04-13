56ef00e068c38de5044e28740e626567b8b7b77f
elnur-elnur <tahirzadee@gmail.com>
2023-04-13 14:40:36 +0400

    created get date / connection db

--- src/components/TimeLogList.js
+++ src/components/TimeLogList.js
@@ -1,17 +1,36 @@
 // src/TimeLogList.js
-import React, { useState, useEffect } from 'react';
-import TimeLog from './TimeLog';
+import React, { useState, useEffect } from "react";
+import TimeLog from "./TimeLog";
+import { db } from "../config/firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 function TimeLogList() {
   const [timeLogs, setTimeLogs] = useState([]);
 
+  const timeLogsCollectionRef = collection(db, "time-logg");
+
   useEffect(() => {
     // TODO: Fetch time logs from Firebase database and set state
+    const getTimeLogs = async () => {
+      try {
+        const data = await getDocs(timeLogsCollectionRef);
+
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setTimeLogs(filteredData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getTimeLogs();
   }, []);
 
   return (
     <div>
-      {timeLogs.map(timeLog => (
+      {timeLogs.map((timeLog) => (
         <TimeLog key={timeLog.id} {...timeLog} />
       ))}
     </div>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDUYcynU9Lid9jToQUxt-qDGDC05J7EM3c",
@@ -12,4 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
