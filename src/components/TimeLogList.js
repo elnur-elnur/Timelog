1a3152da9c79815b3135f36870836a944549f0ff
elnur-elnur <tahirzadee@gmail.com>
2023-04-14 23:58:22 +0400

    added react pagination / added queries / updated sytles

--- src/components/TimeLogList.js
+++ src/components/TimeLogList.js
@@ -1,48 +1,75 @@
-// src/TimeLogList.js
 import React, { useState, useEffect } from "react";
 import TimeLog from "./TimeLog";
 import { db } from "../config/firebase";
-import { getDocs, collection } from "firebase/firestore";
 import moment from "moment";
+import ReactPaginate from "react-paginate";
+
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+  onSnapshot,
+  getDocs,
+} from "firebase/firestore";
 
 function TimeLogList() {
   const [timeLogs, setTimeLogs] = useState([]);
   const [blockTime, setblockTime] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
+  const PER_PAGE = 5;
+  const [lastDoc, setLastDoc] = useState(null);
 
   const timeLogsCollectionRef = collection(db, "time-logg");
-
-  const getTimeLogs = async () => {
-    try {
-      const data = await getDocs(timeLogsCollectionRef);
-
-      const filteredData = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setTimeLogs(filteredData);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const postsQuery = query(
+    timeLogsCollectionRef,
+    orderBy("date", "desc"),
+    limit(PER_PAGE),
+    startAfter(lastDoc)
+  );
 
   useEffect(() => {
+    const getTimeLogs = async () => {
+      try {
+        const data = await getDocs(
+          query(timeLogsCollectionRef, orderBy("date", "desc"))
+        );
+
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setTimeLogs(filteredData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     getTimeLogs();
   }, []);
 
-  useEffect(() => {
-    getTimeLogs();
-  }, [timeLogs]);
+  const handlePageClick = ({ selected: selectedPage }) => {
+    setCurrentPage(selectedPage);
+  };
+
+  const offset = currentPage * PER_PAGE;
+  const currentPageData = timeLogs.slice(offset, offset + PER_PAGE);
 
   useEffect(() => {
-    timeLogs.map((i) => {
-      setblockTime(moment(i.date.seconds * 1000).format("L"));
-    });
-  }, [timeLogs]);
+    if (currentPageData.length > 0) {
+      const firstLogDate = moment(
+        currentPageData[0].date.seconds * 1000
+      ).format("L");
+      setblockTime(firstLogDate);
+    }
+  }, [currentPageData]);
 
   return (
     <div style={{ gridColumn: "1 / 3" }}>
       <h2 className="block_time">{blockTime}</h2>
-      {timeLogs.length > 0 ? (
+      {currentPageData.length > 0 ? (
         <table>
           <thead>
             <tr>
@@ -52,7 +79,7 @@ function TimeLogList() {
             </tr>
           </thead>
           <tbody>
-            {timeLogs.map((timeLog) => (
+            {currentPageData.map((timeLog) => (
               <TimeLog key={timeLog.id} {...timeLog} />
             ))}
           </tbody>
@@ -60,6 +87,17 @@ function TimeLogList() {
       ) : (
         "Yuklenir..."
       )}
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={Math.ceil(timeLogs.length / PER_PAGE)}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        previousLinkClassName={"previous_page"}
+        nextLinkClassName={"next_page"}
+        disabledClassName={"pagination_disabled"}
+        activeClassName={"pagination_active"}
+      />
     </div>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUYcynU9Lid9jToQUxt-qDGDC05J7EM3c",
-  authDomain: "time-logg-app.firebaseapp.com",
-  projectId: "time-logg-app",
-  storageBucket: "time-logg-app.appspot.com",
-  messagingSenderId: "1004929905027",
-  appId: "1:1004929905027:web:4c23387a1bce391dfec6b6",
-  measurementId: "G-KT1SVYTWGL",
+  apiKey: "AIzaSyDVuvdXP-Cry-cAQpduf21bLelisB2eUlg",
+  authDomain: "my-time-logg.firebaseapp.com",
+  projectId: "my-time-logg",
+  storageBucket: "my-time-logg.appspot.com",
+  messagingSenderId: "745491902831",
+  appId: "1:745491902831:web:d5c4f9de84a17128d6789a",
+  measurementId: "G-E7R9BN9529",
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
